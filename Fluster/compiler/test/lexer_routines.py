0919d78d93e2d38419bbe735d5dc4b1d480bf802
Michael Belousov <michael.belousov98@gmail.com>
2018-11-05 15:24:02 -0500

    added example test case

--- Fluster/compiler/test/lexer_routines.py
+++ Fluster/compiler/test/lexer_routines.py
@@ -12,18 +12,36 @@ from textwrap import dedent
 # TODO: maybe define a superclass so all tests have the same interface?
 def load():
     """load the code to be tested"""
-    try:
-        cppyy.load_reflection_info('lib/lexer_routines.so')  # FIXME: load from *.cpp
-        cppyy.include('src/lexer_routines.h')
-        from cppyy.gbl import lexer_routines
-    except OSError as err:  # no such directory
-        print(dedent('''
-                     Test not being run in 
-                     the root directory
-                     '''))
-        raise
+
+class TestReadHex(unittest.TestCase):
+    def __init__(self):
+        try:
+            cppyy.load_reflection_info('lib/lexer_routines.so')  # FIXME: load from *.cpp
+            cppyy.include('src/lexer_routines.h')
+            from cppyy.gbl import lexer_routines
+        except OSError as err:  # no such directory
+            print(dedent('''
+                         Test not being run in 
+                         the root directory
+                         '''))
+            raise
+
+    def test_badcontent(self):
+        """test"""
+        lexer_routines.make_HEXBYTES("0x!2")
+
+    def test_badprefix(self):
+        lexer_routines.make_HEXBYTES("0x!2")
+
+    def test_first100(self):
+        for i in range(100):
+            self.assertEqual(
+                    lexer_routines.make_HEXBYTES(hex(i)),
+                    i.to_bytes())
+
 
     
 if __name__ == '__main__':
     load()
+    unittest.main()
 
