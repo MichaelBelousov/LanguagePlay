76c49c3eef7130f81170f5281d37e6990507efdb
Michael Belousov <michael.belousov98@gmail.com>
2018-11-06 08:26:37 -0500

    completed first trivial C++ test case

--- Fluster/compiler/test/literal_loading.cpp
+++ Fluster/compiler/test/literal_loading.cpp
@@ -3,16 +3,36 @@
  */
 
 #include <iostream>
+#include <cstdio>
 
+#include "lexer_routines.h"
+#include "parser.h"
 
-//returns a flag word where each set bit is a failed test
-int runTests {
-    int failure_word;
-    auto record_result = [&failure_word](bool result) -> void
-        { failure_word = (failure_word << 1) | static_cast<int>(result) };
+int assert(bool result, const std::string& msg)
+{
+    if (!result)
+    {
+        std::cerr << msg << std::endl;
+        exit(100);
+    }
+}
+
+bool test_hex_standard1() {
+    //still need to decide on endianness...
+    char expected[] = {0x0a, 0x0f,0x5c};
+    auto actual= make_HEXBYTES("0xa0f5c")
+    for (int i = 0; i < 3; ++i)
+        if (actual[i] != expected[i])
+        {
+                std::cerr << actual[i] 
+                          << " != " 
+                          << expected[i] 
+                          << std::endl;
+                return false;
+        }
 }
 
 int main(int argc, char* argv[])
 {
-    return runTests();
+    assert(test_hex_standard1(), std::string("failed loading 0xa0f5c"));
 }
