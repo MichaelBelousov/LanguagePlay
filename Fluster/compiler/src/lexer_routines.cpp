0ac53f2187ecf246474be72c3cbe848ff537842f
Michael Belousov <michael.belousov98@gmail.com>
2018-11-06 09:36:49 -0500

    Almost done writing generic byte loading routine

--- Fluster/compiler/src/lexer_routines.cpp
+++ Fluster/compiler/src/lexer_routines.cpp
@@ -1,23 +1,86 @@
 
-  yy::parser::symbol_type 
-  make_DECIMINTEGER (const std::string &s, const yy::parser::location_type& loc);
-  yy::parser::symbol_type 
-  make_DECIMFLOAT(const std::string &s, const yy::parser::location_type& loc);
-  yy::parser::symbol_type 
-  make_HEXBYTES (const std::string &s, const yy::parser::location_type& loc);
-  yy::parser::symbol_type 
-  make_BINARYBITS (const std::string &s, const yy::parser::location_type& loc);
-  yy::parser::symbol_type 
-  make_OCTALBITS (const std::string &s, const yy::parser::location_type& loc);
-  yy::parser::symbol_type 
-  make_STRING (const std::string &s, const yy::parser::location_type& loc);
-  yy::parser::symbol_type 
-  make_UNICODESTRING (const std::string &s, const yy::parser::location_type& loc);
-  yy::parser::symbol_type 
-  make_RAWSTRING (const std::string &s, const yy::parser::location_type& loc);
+#include <cmath>
+
+struct BitBuffer
+{
+    int length;
+    char* bytes;
+    BitVuffer(int in_bits)
+        : length(in_bits)
+        , bytes(new char[std::ceil(in_bits/8.f)])
+    {}
+    ~BitBuffer() { delete[] bytes; }
+};
+
+template<int base>
+BitBuffer loadAsciiBitLiteral(const std::string& literal, const std::string& prefix)
+{
+    static_assert(base <= 36, "only 26 letters + 10 numbers supported");
+
+    const int bits_per_char = std::log2(base);
+    const int bit_amt = (literal.length() - prefix.length()) * bits_per_char;
+    const int byte_amt = ceil(bit_amt / 8);
+
+    auto result = BitBuffer(bit_amt);
+
+    auto setBits = [&result, &bits_per_char]
+            (unsigned bit_offset, int value) -> void 
+    {
+        const unsigned byte_offset = bit_offset / 8;
+        const unsigned inner_bit_offset = bit_offset % 8;
+        if (inner_bit_offset < bits_per_char)
+        wordval |= (p[i] - 48) << depth*bits_per_char;
+    };
+
+    int depth = 0; //initialize depth to first bit to set
+    int depth = byte_amt/ bit_amt
+    for (auto itr = literal.cbegin(); 
+         itr != literal.cend();
+         ++itr, depth+=bits_per_char)
+    { 
+        if (p[i] <= 57) //is an ascii number
+            wordval |= (p[i] - 48) << depth*bits_per_char;
+        else            //is an ascii letter
+            wordval |= ((p[i] | (1<<5)) - 87) << depth*bits_per_char;
+        byte = val;
+
+        if (depth == bits_per_char-1) {
+            head[i] = word[0];
+            head[i+1] = word[1];
+            head[i+2] = word[2];
+            wordval = 0;
+            head += buffsize;
+        }
+    }
+    //FIXME: optimize using a lambda or (god-forbid) a **label+goto**
+    if (depth) {
+        //FIXME: switch to full 32-bit word load
+        head[i] = word[0];
+        head[i+1] = word[1];
+        head[i+2] = word[2];
+        wordval = 0;
+        head += 3;
+    }
+    return result;
+}
+
+yy::parser::symbol_type 
+make_DECIMINTEGER (const std::string &s, const yy::parser::location_type& loc)
+{
+    errno = 0;
+    long long n = strtoll (s.c_str(), NULL, 10);
+    if (! (INT_MIN <= n && n <= INT_MAX && errno != ERANGE))
+        throw yy::parser::syntax_error (loc, "integer is out of range: " + s);
+    return yy::parser::make_NUMBER (n, loc);
+}
+
+yy::parser::symbol_type 
+make_DECIMFLOAT(const std::string &s, const yy::parser::location_type& loc);
 
 /* byte literals */
-0x{hex}*    {
+yy::parser::symbol_type 
+make_HEXBYTES (const std::string &s, const yy::parser::location_type& loc)
+{
     char bytes[] = new char[ceil((strlen(text())-2)/2.0)];
     bool do_load = false; //could move this to the oddness of i
     char byte;
@@ -39,14 +102,13 @@
             byte |= val << 4;  //add the more significant nibble
             bytes[i/2] = byte;  //relying on floor division
         }
-        
     }
     yylval.bytes = bytes;
     return toks::lits::byte_;
 }
-
-/* bit literal */
-0b{bit}*    {
+yy::parser::symbol_type 
+make_BINARYBITS (const std::string &s, const yy::parser::location_type& loc)
+{
     //FIXME: untested and I may have loaded the byte stream backwards
     //just use rshift and reverse the process. Maybe make all this
     //repetitive code into a procedure, too
@@ -70,8 +132,9 @@
     return toks::lits::bit_;
 }
 
-/* octal literal */
-0o{octal}   {
+yy::parser::symbol_type 
+make_OCTALBITS (const std::string &s, const yy::parser::location_type& loc)
+{
     //FIXME: untested and I may have loaded the byte stream backwards
     //TODO: account for endianness
     char bytes[] = new char[ceil((strlen(text())-2)/3.0)];
@@ -100,59 +163,14 @@
         wordval = 0;
         head += 3;
     }
-    yylval.bytes = bytes;
-    return toks::lits::byte_;
+    return bytes;
 }
 
-/* string literal */
-\"{string_content}\"                        return toks::lits::string_;
+//yy::parser::symbol_type 
+//make_STRING (const std::string &s, const yy::parser::location_type& loc);
 
-/* unicode literal */
-u\"(?u:{string_content})\"                  return toks::lits::string_unicode_;
+//yy::parser::symbol_type 
+//make_UNICODESTRING (const std::string &s, const yy::parser::location_type& loc);
 
-/* raw string literal */
-\`{string_content}\`                        return toks::lits::string_raw_;
-
-
-
-/*** miscellanious ***/
-
-
-/* multiline comment */
-"/*"(.|\n)*?"*/"                            //ignore
-
-/* eol comment */
-"//".*                                      //ignore
-
-/* eof */
-<<EOF>>                                     return -1;
-
-
-
-/*** indentation ***/
-
-^\h*\i          return toks::spc::indent;
-
-^\h*\j          return toks::spc::outdent;
-
-\j              return toks::spc::outdent;
-
-(?^\\\n\h*)     //ignore space after line-end backslash
-
-\\              return toks::spc::indent; //add indent after in-line backslash
-
-
-/* ********************************************************************************** */ 
-/* ********************************************************************************** */ 
-/* ********************************************************************************** */ 
-%%
-/* ********************************************************************************** */ 
-/* ********************************************************************************** */ 
-/* ********************************************************************************** */ 
-
-
-int main()
-{
-    return Lexer().lex();
-}
-
+//yy::parser::symbol_type 
+//make_RAWSTRING (const std::string &s, const yy::parser::location_type& loc);
