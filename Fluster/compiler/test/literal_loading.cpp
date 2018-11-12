04607a68bc78040ef63fb6d8d2b61300ae4ab9b1
Michael Belousov <michael.belousov98@gmail.com>
2018-11-12 15:21:49 -0500

    tweaking code, actually working out the problem would be better

--- Fluster/compiler/test/literal_loading.cpp
+++ Fluster/compiler/test/literal_loading.cpp
@@ -48,32 +48,31 @@ void setBits(unsigned bit_offset, unsigned int bits_len, unsigned char bits, uns
 {
     using cuint = const unsigned;
     cuint left_byte_id = bit_offset/8;
-    std::cout << "left_byte_id"  << left_byte_id;
     cuint left_bound = bit_offset % 8;
-    std::cout << "left_bound"  << left_bound;
-    cuint right_byte_id = (bit_offset+bits_len)/8;
-    std::cout << "right_byte_id"  << right_byte_id;
-    cuint right_bound = (bit_offset+bits_len) % 8;
-    std::cout << "right_bound"  << right_bound;
-    std::cout << Bits(bits) << " ";
+    cuint right_byte_id = (bit_offset-1+bits_len)/8;
+    cuint right_bound = (bit_offset-1+bits_len) % 8;
 
     if (left_byte_id == right_byte_id)
     {
-        bytes[right_byte_id] |= bits << right_bound;
-        std::cout << Bits(bytes[left_byte_id]) 
-                 << std::endl;
+        bytes[right_byte_id] |= bits << (8-(right_bound+1));
+        std::cout 
+            << Bits(8-(right_bound)+1) << " "
+            << Bits(bytes[right_byte_id])
+            << std::endl;
     }
     else
     {
-        bytes[right_byte_id] |= bits << right_bound;
-        bytes[left_byte_id] |= bits >> (8-left_bound);
-        std::cout << Bits(bytes[left_byte_id]) 
-                 << Bits(bytes[right_byte_id])
-                 << std::endl;
+        bytes[right_byte_id] |= bits << (8-(right_bound+1));
+        bytes[left_byte_id] |= bits >> (left_bound - bits_len);
     }
+    std::cout 
+        << "right " << right_byte_id << ", left " << left_byte_id
+        << "r] " << right_bound << ", l[ " << left_bound
+        << std::endl;
 }
 
 
+//TODO: bake prefix into template param? too bad it's a string...
 template<int base>
 BitBuffer loadAsciiBitLiteral(const std::string& literal, const std::string& prefix)
 {
@@ -81,30 +80,63 @@ BitBuffer loadAsciiBitLiteral(const std::string& literal, const std::string& pre
 
     const int bits_per_char = std::log2(base);
     const int bit_amt = (literal.length() - prefix.length()) * bits_per_char;
-    const int byte_amt = std::ceil(bit_amt / 8);
+    const int byte_amt = std::ceil(bit_amt/8.f);
 
     auto result = BitBuffer(bit_amt);
 
-    int depth = 0;
+    const int bit_padding = (byte_amt*8) - bit_amt;
+    int depth = bit_padding;
     for (auto itr = literal.cbegin()+prefix.length(); 
          itr != literal.cend();
          ++itr, depth+=bits_per_char)
     { 
-        //std::cout << result << std::endl;
         unsigned char bits;
         if (*itr  <= 57) //is an ascii number
              bits = *itr  - 48;
         else            //is an ascii letter
             bits = (*itr | (1<<5)) - 87;
-        std::cout << *itr << " " << static_cast<int>(bits) << " ";
         setBits(depth, bits_per_char, bits, result.bytes);
     }
     return result;
 }
 
-auto make_HEXBYTES = [](const std::string& literal){
-    return loadAsciiBitLiteral<16>(literal, "0x");
-};
+bool test_hex_standard1() {
+    //still need to decide on endianness...
+    std::cout << ("testing 0x0a0f5c...") << std::endl;
+    bool result = true;
+    unsigned char expected[] = {0x0a, 0x0f, 0x5c};
+    auto actual = loadAsciiBitLiteral<16>("0x0a0f5c", "0x");
+    for (int i = 0; i < 3; ++i)
+        if (actual.bytes[i] != expected[i])
+        {
+            std::cerr << Bits(actual.bytes[i])
+                      << " != " 
+                      << Bits(expected[i])
+                      << std::endl;
+            result = false;
+        }
+    if (result) std::cout << ("test successful") << std::endl;
+    return result;
+}
+
+bool test_oct_standard1() {
+    std::cout << ("testing 0o777...") << std::endl;
+    bool result = true;
+    unsigned char expected[] = {01, 0377};
+    auto actual = loadAsciiBitLiteral<8>("0o777", "0o");
+    for (int i = 0; i < 2; ++i)
+        if (actual.bytes[i] != expected[i])
+        {
+            std::cerr << "byte " << i << ": "
+                      << Bits(actual.bytes[i])
+                      << " != " 
+                      << Bits(expected[i])
+                      << std::endl;
+            result = false;
+        }
+    if (result) std::cout << ("test successful") << std::endl;
+    return result;
+}
 
 int do_assert(bool result, const std::string& msg)
 {
@@ -116,31 +148,15 @@ int do_assert(bool result, const std::string& msg)
     return 0;
 }
 
-bool test_hex_standard1() {
-    //still need to decide on endianness...
-    std::cout << ("0x0a0f5c") << std::endl;
-    unsigned char expected[] = {0x0a, 0x0f, 0x5c};
-    auto actual = make_HEXBYTES("0x0a0f5c"); //a0f5c
-    for (int i = 0; i < 3; ++i)
-        if (actual.bytes[i] != expected[i])
-        {
-            std::cout << "actual: " 
-                << Bits(actual.bytes[i]) 
-                << std::endl;
-            std::cout << "expected: " 
-                << Bits(expected[i])
-                << std::endl;
-            std::cerr << static_cast<int>(actual.bytes[i])
-                      << " != " 
-                      << static_cast<int>(expected[i])
-                      << std::endl;
-            //return false;
-        }
-    return true;
-}
 
 int main(int argc, char* argv[])
 {
-    do_assert(test_hex_standard1(), 
-            std::string("failed loading 0xa0f5c"));
+    int failed = 0;
+    failed += do_assert(test_hex_standard1(), 
+                        "failed loading 0xa0f5c");
+    failed += do_assert(test_oct_standard1(), 
+                        "failed loading 0o777");
+
+    if (!failed)
+        std::cout << "passed all tests" << std::endl;
 }
